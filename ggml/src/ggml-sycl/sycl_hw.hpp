2985be33242084acfe8a5c1c0f92ab08bab61a31
arthw <zhang.jianyu@outlook.com>
2026-03-31 09:24:40 +0800

    update hw info

--- ggml/src/ggml-sycl/sycl_hw.hpp
+++ ggml/src/ggml-sycl/sycl_hw.hpp
@@ -11,11 +11,26 @@
 namespace syclex = sycl::ext::oneapi::experimental;
 using gpu_arch = sycl::ext::oneapi::experimental::architecture;
 
+// It's used to mark the GPU computing capacity in Flash-attention OP
+// The value must flow the order of performance.
+enum sycl_intel_gpu_family {
+  GPU_FAMILY_UKNOWN = -1,
+  // Normal iGPU in Intel Core CPU, before Meteor Lake iGPU(Xe)
+  GPU_FAMILY_IGPU_NON_XE = 0,
+  // iGPU with Xe core, Meteor Lake iGPU or newer.
+  GPU_FAMILY_IGPU_XE = 1,
+  // dGPU for gaming in client/data center, DG1/FLex 140 or newer.
+  GPU_FAMILY_DGPU_CLIENT_GAME = 2,
+  // dGPU for AI in cloud, PVC or newer.
+  GPU_FAMILY_DGPU_CLOUD = 3
+};
+
 struct sycl_hw_info {
   syclex::architecture arch;
   const char* arch_name;
   int32_t device_id;
   std::string name;
+  sycl_intel_gpu_family gpu_family;
 };
 
 sycl_hw_info get_device_hw_info(sycl::device *device_ptr);
